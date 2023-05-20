baa49196a83e6a5888bad7da07c518e196cff9dc
VinnyXP <viph0009@gmail.com>
2023-05-19 19:57:15 -0700

    Update: proper Egg Dash

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 import pandas as pd
 import plotly.express as px
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 
 app = Dash(__name__)
+app.title = 'Egg Dash'
 
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+data = (
+    pd.read_csv('eggs.csv')
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+egg_year = data.columns[1:]
+dropdown_options = [{'label': year, 'value': year} for year in egg_year]
+
+@app.callback(
+    Output('bar-graph', 'figure'),
+    Input('year-dropdown', 'value')
+)
+def update__bar_graph(selected_year):
+    filtered_df = data[['State/Union Territory', selected_year]]
+    fig = px.bar(filtered_df, x='State/Union Territory', y=selected_year, barmode="group", text_auto=True,  title=f"Egg Production {selected_year}")
+    fig.update_layout(autotypenumbers='convert types')
+    fig.update_layout(barmode='stack', xaxis={'categoryorder': 'total ascending'})
+    fig.update_traces(width=0.8)
+    return fig
+
+app.layout = html.Div(
+    children=[
     html.H1(
-        children='Hello Dash',
+        children='Egg Production in India',
         style={
-            'textAlign': 'center',
-            'color': colors['text']
+            'textAlign': 'center'
         }
     ),
-
-    html.Div(children='Dash: A web application framework for your data.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
+    html.Div(children='''
+        Number of eggs produced in yearly intervals.
+    '''),
+    dcc.Dropdown(
+        id='year-dropdown',
+        options=dropdown_options,
+        value=egg_year[0] 
+    ),
+    html.Div(
+        dcc.Graph(id='bar-graph')
     )
-])
-
+]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
